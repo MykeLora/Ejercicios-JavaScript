067dfe981bbd6b4e8593775097c7ffbb0f54f9ba
MykeLora <maycollora16@gmail.com>
2023-12-31 22:00:28 -0600

    Optimizando el código de manera más sencilla

--- Controles_form_button_text/calculadora-basica/calculadora.js
+++ Controles_form_button_text/calculadora-basica/calculadora.js
@@ -1,53 +1,40 @@
-function sumar() {
+function calcular(operacion){
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
 
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
+    if(isNaN(num1) && isNaN(num2)){
+        document.getElementById('resultado').innerText = 'Ingresa números validos';
 
-    if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById('resultado').innerText = 'Ingresa números válidos';
-    } else {
-        var suma = num1 + num2;
-        document.getElementById('resultado').innerText = suma.toLocaleString();
+    }else{
+        var resultado;
+
+        switch(operacion){
+            case 'suma':
+                resultado = num1 + num2;
+                break;
+
+            case 'resta':
+                resultado = num1 - num2;
+                break;
+        
+            case 'producto':
+                resultado = num1 * num2;
+                break;
+
+            case 'dividir':
+                if(num2 !== 0){
+                    resultado = num1 / num2;  
+                }else{
+                    document.getElementById('resultado').innerText = 'No es posible dividir por cero';
+                    return;
+                }
+                break;
+
+            default:
+                document.getElementById('resultado').innerText = 'Operación no válida';
+                return;
+                       
+        }
+        document.getElementById('resultado').innerText = resultado.toLocaleString();
     }
 }
-
-function resta(){
-
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-
-    if (isNaN(num1) || isNaN(num2)){
-        document.getElementById('resultado').innerText = 'Asegúrese de utilizar números positivos';
-    } else {
-        var resta = num1 - num2;
-        document.getElementById('resultado').innerText = resta.toLocaleString();
-    }
-}
-
-function producto(){
-
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-
-    if (isNaN(num1) || isNaN(num2)){
-        document.getElementById('resultado').innerText = 'Asegúrese de utilizar números positivos';
-    } else {
-        var producto = num1 * num2;
-        document.getElementById('resultado').innerText = producto.toLocaleString();
-    }
-}
-
-function dividir(){
-
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-
-
-    if (isNaN(num1) || isNaN(num2)){
-        document.getElementById('resultado').innerText = 'Asegúrese de utilizar números positivos';
-    } else {
-        var division = num1 / num2;
-        document.getElementById('resultado').innerText = division.toLocaleString();
-    }
-}
-
